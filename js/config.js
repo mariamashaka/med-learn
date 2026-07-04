1a247c5ac8800cb876297d3ad54c000240a3b214
mariamashaka <maria.mashaka.chacha@gmail.com>
2026-07-04 18:50:03 +0300

    Update Supabase URL and ANON key in config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://ТВОЙ_ПРОЕКТ.supabase.co';
-const SUPABASE_ANON_KEY = 'ТВОЙ_ANON_KEY';
+const SUPABASE_URL = 'https://oemyyvlzbagllmqkdhat.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable__shpWgxygHxXwpUVq94zvA_n0J7rmui';
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
